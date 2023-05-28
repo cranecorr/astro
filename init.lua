ed7a1ae0b636ae9028ae4406208f746534c4ec94
Andrew Corr <cranecorr@gmail.com>
2023-05-28 14:46:49 +0300

    First configuration: add <leader>rt for run python files in terminal whith indent mode

--- init.lua
+++ init.lua
@@ -19,6 +19,8 @@ return {
 
   -- Set colorscheme to use
   colorscheme = "astrodark",
+  vim.cmd "autocmd TermOpen * startinsert",
+
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
